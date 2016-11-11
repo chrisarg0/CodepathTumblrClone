2b0d6bbc37d55df3e47262fcbe5658e0b8f92b49
chrisarg0 <cargonish22@gmail.com>
2016-11-10 18:53:55 -0700

    updates to storyboard

--- Tumblr Clone/ViewController.swift
+++ Tumblr Clone/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello tumblr")
